6fdaa0d262cf38322d59b2f21df87459c4746cc2
VikashMCA115f:\fsd\fdata-structure\index.html <vikasgayatriyadav@gmail.com>
2024-07-24 19:40:52 +0530

    resume.html

---
+++ DowhileLoop.java
@@ -0,0 +1,10 @@
+public class DowhileLoop {
+    public static void main(String args[]) {
+        int i = 1, j;
+        do {
+            j = 2 * i;
+            System.out.println("2" + " * " + i + " = " + j);
+            i++;
+        } while (i <= 10);
+    }
+}
